8da6bac976fc03a23d0fd851739ecc48a5a6cb80
sj0404k <ksj06030@naver.com>
2023-11-14 09:01:42 +0900

    업뎃

--- android/app/src/mycalendar/ExerciseGraph.js
+++ android/app/src/mycalendar/ExerciseGraph.js
@@ -1,54 +1,69 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { BarChart } from 'react-native-chart-kit';
 
-const screenWidth = Dimensions.get('window').width - 8;
+const screenWidth = Dimensions.get('window').width;
 
 const data = {
-  labels: ['월', '화', '수', '목', '금', '토', '일'],
+  labels: ['월', '화', '수', '목', '금', '토', '일', ''],
   datasets: [
     {
-      data: [20, 45, 28, 80, 99, 43, 50],
+      data: [20, 45, 28, 80, 99, 43, 50, 0],
     },
   ],
 };
 
 const ExerciseGraph = () => {
+  const formatYLabel = (value) => `$${value}`;
   return (
-    <View style={{ padding: 1, }}>
-      <Text style={styles.title}> 운동시간 그래프</Text>
+    <ScrollView
+      horizontal
+      contentContainerStyle={styles.container}
+      showsHorizontalScrollIndicator={false}
+    >
       <View>
-        <BarChart
-          data={data}
-          width={screenWidth}
-          height={200}
-          yAxisLabel="$"
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            decimalPlaces: 2,
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          }}
-          style={{
-            marginVertical: 2,
-            borderRadius: 16,
-            padding: 1,
-          }}
-        />
+        <Text style={styles.title}> 운동시간 그래프</Text>
+        <View>
+          <BarChart
+            data={data}
+            width={screenWidth}
+            height={200}
+            yAxisSuffix="분"
+
+            chartConfig={{
+              backgroundColor: '#ffffff',
+              backgroundGradientFrom: 'gray',
+              backgroundGradientTo: '#000000',
+              decimalPlaces: 0,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              barPercentage: 1.3,
+
+            }}
+            style={{
+              marginVertical: 2,
+              borderRadius: 16,
+              padding: 0,
+            }}
+            formatYLabel={formatYLabel}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
     color: 'black',
     fontSize: 24,
     fontFamily: 'Noto Sans',
     fontWeight: '700',
-    wordWrap: 'break-word'
   }
 })
 
--- android/app/src/mycalendar/ExerciseRecord.js
+++ android/app/src/mycalendar/ExerciseRecord.js
@@ -1,25 +1,66 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { BarChart } from 'react-native-chart-kit';
 
 const screenWidth = Dimensions.get('window').width - 8;
 
+
 const ExerciseRecord = () => {
+
+  const exerciseData = [
+    { exerciseType: '스쿼트', time: '1분00초', sets: '3세트',Reps:'30초' },
+    { exerciseType: '벤치 프레스', time: '5분30초', sets: '4세트',Reps:'10초' },
+    // 다른 운동 데이터 추가
+  ];
+
   return (
-    <View style={{ padding: 1, }}>
-      <Text style={styles.title}> 운동 기록</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>운동 기록</Text>
+      <View style={styles.table}>
+        <View style={styles.row}>
+          <Text style={styles.cell}>운동 이름</Text>
+          <Text style={styles.cell}>운동 시간</Text>
+          <Text style={styles.cell}>세트</Text>
+          <Text style={styles.cell}>휴식</Text>
+        </View>
+        {exerciseData.map((data, index) => (
+          <View key={index} style={styles.row}>
+            <Text style={styles.cell}>{data.exerciseType}</Text>
+            <Text style={styles.cell}>{data.time}</Text>
+            <Text style={styles.cell}>{data.sets}</Text>
+            <Text style={styles.cell}>{data.Reps}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
   title: {
     color: 'black',
     fontSize: 24,
     fontFamily: 'Noto Sans',
     fontWeight: '700',
-    wordWrap: 'break-word'
-  }
-})
+  },
+  table: {
+    flexDirection: 'column',
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  row: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+  },
+  cell: {
+    flex: 1,
+    padding: 8,
+    textAlign: 'center',
+  },
+});
 
 export default ExerciseRecord;
